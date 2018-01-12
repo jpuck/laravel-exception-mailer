e828cf606188a98c859f89ac23c7538658d069b2
Jeff Puckett <jeff@jeffpuckett.com>
2018-01-12 03:51:49 +0000

    add config file

--- src/Provider.php
+++ src/Provider.php
@@ -8,6 +8,11 @@ class Provider extends ServiceProvider
 {
     public function boot()
     {
+        $config = __DIR__.'/../config/exception-mailer.php';
+        $this->publishes([
+            $config => config_path('exception-mailer.php'),
+        ]);
+
         $this->loadViewsFrom(__DIR__.'/../views', 'exception-mailer');
     }
 }
